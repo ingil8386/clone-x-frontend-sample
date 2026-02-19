43ef1395a97af11c8de894f97a2e89107f537d2f
ingil8386 <soo_os@naver.com>
2026-02-19 14:19:25 +0900

    #2 Init LoginPage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <h2>Hello Wolrd! hello git</h2>
+  <div class="container">
+  <LoginPage></LoginPage>
+  </div>
 
 </template>
 
 <script>
-
+import LoginPage from './views/LoginPage.vue';
 export default {
-  name: 'App',
+  name: "App",
+  components : { LoginPage},
+
 }
 </script>
 
 <style>
+body{
+  background-color: black;
+  color: white;
+}
 
+.container{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 </style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="text" :placeholder="'Email'" v-model="text">
+     <input type="password" :placeholder="'Password'" v-model="password">
+  </div>
+<button @click="login">로그인하기</button>
+
+<div>
+    계정이 없으신가요?
+    <span>가입하기</span>
+</div>
+</template>
+
+<script>
+export default {
+    name:"LoginPage",
+    data(){
+        return {
+            email:"",
+            password:"",
+        };
+    },
+    methods: {
+        login(){
+            console(this.email, this.password);
+        },
+    },
+};
+</script>
+
+<style>
+
+</style>
